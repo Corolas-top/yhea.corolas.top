450fba7af83208bc77557813cd68e00484f9d16f
Rhea_Corolas <junjiegaopersonal@gmail.com>
2026-04-22 00:27:19 +0800

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from '@/types/supabase';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || '';
-const supabaseKey = import.meta.env.VITE_SUPABASE_SUPBLISHABLE_KEY || '';
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY || '';
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
