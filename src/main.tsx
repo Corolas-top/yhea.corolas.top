eeeea1a61a25e4951dc631e82e4f994570e80f6b
Rhea_Corolas <junjiegaopersonal@gmail.com>
2026-04-24 22:57:25 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { AuthProvider } from './hooks/useAuth';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HashRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </HashRouter>
   </StrictMode>
 );
